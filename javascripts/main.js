5ab50380de20622d3083ba1e60589de3086d3719
daniel <drward3@uno.edu>
2013-04-24 09:08:02 -0500

    added the demo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+$(document).ready(function(){
+	$('#reader').html5_qrcode(function(data){
+			$('#read').html(data);
+		},
+		function(error){
+			$('#read_error').html(error);
+		}, function(videoError){
+			$('#vid_error').html(videoError);
+		}
+	);
+});
