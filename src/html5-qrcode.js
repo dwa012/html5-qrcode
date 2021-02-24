9f79957ab81a9885774b36e4f59f81b212e881d9
Gabriela Linck <gabriela.linck@outlook.com>
2021-02-24 09:20:58 -0300

    Navigator.getUserMedia() is deprecated (#68)

--- src/html5-qrcode.js
+++ src/html5-qrcode.js
@@ -15,7 +15,7 @@
                     width = 300;
                 }
 
-                var vidElem = $('<video width="' + width + 'px" height="' + height + 'px"></video>').appendTo(currentElem);
+                var vidElem = $('<video width="' + width + 'px" height="' + height + 'px" autoplay playsinline></video>').appendTo(currentElem);
                 var canvasElem = $('<canvas id="qr-canvas" width="' + (width - 2) + 'px" height="' + (height - 2) + 'px" style="display:none;"></canvas>').appendTo(currentElem);
 
                 var video = vidElem[0];
@@ -41,26 +41,18 @@
                 };//end snapshot function
 
                 window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
-                navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
-                var successCallback = function(stream) {
-                    video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
+
+                navigator.mediaDevices.getUserMedia({video: { facingMode: "environment" }}).then(function successCallback(stream) {
+                    video.srcObject = stream;
                     localMediaStream = stream;
                     $.data(currentElem[0], "stream", stream);
 
                     video.play();
                     $.data(currentElem[0], "timeout", setTimeout(scan, 1000));
-                };
-
-                // Call the getUserMedia method with our callback functions
-                if (navigator.getUserMedia) {
-                    navigator.getUserMedia({video: true}, successCallback, function(error) {
-                        videoError(error, localMediaStream);
-                    });
-                } else {
-                    console.log('Native web camera streaming (getUserMedia) not supported in this browser.');
-                    // Display a friendly "sorry" message to the user
-                }
+                }).then(function(error) {
+                    videoError(error, localMediaStream);
+                });
 
                 qrcode.callback = function (result) {
                     qrcodeSuccess(result, localMediaStream);
